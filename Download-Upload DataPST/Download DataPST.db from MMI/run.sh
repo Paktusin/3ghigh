b519767d8a79bb9eff674d6956c2fe7101f1308f
Mikhail <m.davlichin@upgate.com>
2026-09-10 12:41:15 +0300

    add scripts

---
+++ Download-Upload DataPST/Download DataPST.db from MMI/run.sh
@@ -0,0 +1,29 @@
+#!/bin/ksh
+sdcard=`ls /mnt|grep sdcard.*t`
+echo Using card $sdcard
+
+dstPath=/mnt/$sdcard
+
+mount -u $dstPath
+echo remounted for full access
+
+$dstPath/utils/showScreen $dstPath/screens/scriptStart.png
+
+rm -f  $dstPath/.done
+
+echo started > $dstPath/.started
+
+
+echo Copying fs0 to $dstPath
+
+ls /mnt/ >$dstPath/.listing
+# место для ваших комманд
+
+cp -v -r /mnt/efs-persist/DataPST.db $dstPath/read/
+
+
+$dstPath/utils/showScreen $dstPath/screens/scriptDone.png
+
+echo done > $dstPath/.done
+
+rm -f  $dstPath/.started
